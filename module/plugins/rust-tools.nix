242da5027327639a93ce6bad9455bb279c350067
zms <root@zms.dev>
2024-09-09 16:42:28 -0400

    Try to fix rust inlay hints.

--- module/plugins/rust-tools.nix
+++ module/plugins/rust-tools.nix
@@ -1,50 +1,38 @@
 {
     plugins.rust-tools = {
         enable = true;
+        reloadWorkspaceFromCargoToml = true;
         inlayHints = {
             auto = true;
-            showParameterHints = false;
-            parameterHintsPrefix = "";
-            otherHintsPrefix = "";
+            onlyCurrentLine = false;
+            showParameterHints = true;
+            parameterHintsPrefix = "<- ";
+            otherHintsPrefix = "=> ";
+            maxLenAlign = false;
+            maxLenAlignPadding = 1;
+            rightAlign = false;
+            rightAlignPadding  = 7;
+            highlight = "Comment";
+        };
+        hoverActions = {
+            border = [
+                ["╭" "FloatBorder"]
+                ["─" "FloatBorder"]
+                ["╮" "FloatBorder"]
+                ["│" "FloatBorder"]
+                ["╯" "FloatBorder"]
+                ["─" "FloatBorder"]
+                ["╰" "FloatBorder"]
+                ["│" "FloatBorder"]
+            ];
+            maxWidth = null;
+            maxHeight = null;
+            autoFocus = false;
         };
         server = {
             lens.enable = true;
             procMacro.enable = true;
-            check = {
-                allTargets = false;
-                command = "clippy";
-            };
-            inlayHints = {
-                lifetimeElisionHints = {
-                    enable = "always";
-                    useParameterNames = false;
-                };
-                reborrowHints = {
-                    enable = "always";
-                };
-                bindingModeHints = {
-                    enable = false;
-                };
-                chainingHints = {
-                    enable = false;
-                };
-                closingBraceHints = {
-                    enable = false;
-                    minLines = 25;
-                };
-                closureReturnTypeHints = {
-                    enable = "never";
-                };
-                parameterHints = {
-                    enable = false;
-                };
-                renderColons = true;
-                typeHints = {
-                    enable = false;
-                    hideClosureInitialization = false;
-                    hideNamedConstructor = false;
-                };
-            };
+            standalone = true;
         };
     };
 }
